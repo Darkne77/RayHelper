fe7ac3d20cb021697335a2ff468dc3769f632472
Leo <rumiantsev.l@gmail.com>
2021-10-23 20:27:46 +0300

    Init Xamarin.Forms.Maps

--- RayHelper/RayHelper.Android/MainActivity.cs
+++ RayHelper/RayHelper.Android/MainActivity.cs
@@ -19,6 +19,7 @@ namespace RayHelper.Android
 
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Xamarin.FormsMaps.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
     }
