a63bdcab1c3414cdd5e3f4726bf4bf2155d17181
leorumyantsev <rumiantsev.l@gmail.com>
2021-11-06 15:31:18 +0300

    Add UsersRankPage and remove unused search icon from iOS and Android

---
+++ RayHelper/RayHelper/UsersRankPage.xaml.cs
@@ -0,0 +1,14 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RayHelper
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UsersRankPage : ContentPage
+    {
+        public UsersRankPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
