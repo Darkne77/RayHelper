93085b237290f961eeffe07f5adf61901f4781c3
Leonid <rumiantsev.l@gmail.com>
2021-11-06 14:44:52 +0300

    Add Shell BackgroundColor and Add icon for Hospice List tab

--- RayHelper/RayHelper.Android/Resources/Resource.designer.cs
+++ RayHelper/RayHelper.Android/Resources/Resource.designer.cs
@@ -21719,13 +21719,19 @@ namespace RayHelper.Android
 			public const int preference_list_divider_material = 2131165325;
 			
 			// aapt resource value: 0x7F07008E
-			public const int test_custom_background = 2131165326;
+			public const int search_gray = 2131165326;
 			
 			// aapt resource value: 0x7F07008F
-			public const int tooltip_frame_dark = 2131165327;
+			public const int search_orange = 2131165327;
 			
 			// aapt resource value: 0x7F070090
-			public const int tooltip_frame_light = 2131165328;
+			public const int test_custom_background = 2131165328;
+			
+			// aapt resource value: 0x7F070091
+			public const int tooltip_frame_dark = 2131165329;
+			
+			// aapt resource value: 0x7F070092
+			public const int tooltip_frame_light = 2131165330;
 			
 			static Drawable()
 			{
