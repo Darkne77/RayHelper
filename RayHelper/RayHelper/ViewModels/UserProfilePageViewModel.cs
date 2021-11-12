2099582c359840f150d644cfae2cb1c523dd7b44
Leonid <rumiantsev.l@gmail.com>
2021-11-12 13:07:29 +0300

    Add authorization panel for UserProfilePage.xaml

--- RayHelper/RayHelper/ViewModels/UserProfilePageViewModel.cs
+++ RayHelper/RayHelper/ViewModels/UserProfilePageViewModel.cs
@@ -2,6 +2,18 @@ namespace RayHelper.ViewModels
 {
     public class UserProfilePageViewModel : MainViewModel
     {
-        
+        private bool _isUserAuthorized;
+        public bool IsUserAuthorized
+        {
+            get => _isUserAuthorized;
+            set => SetProperty(ref _isUserAuthorized, value);
+        }
+
+        public bool IsUserNotAuthorized => !IsUserAuthorized;
+
+        public UserProfilePageViewModel()
+        {
+            IsUserAuthorized = true;
+        }
     }
 }
