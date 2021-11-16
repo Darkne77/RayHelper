94423f58e499596a5bb9a8548ecd9ec1173e2e05
Leo <rumiantsev.l@gmail.com>
2021-11-16 14:46:55 +0300

    Add OpenAboutUsLinkCommand to RayProfilePageViewModel

--- RayHelper/RayHelper/ViewModels/RayProfilePageViewModel.cs
+++ RayHelper/RayHelper/ViewModels/RayProfilePageViewModel.cs
@@ -1,7 +1,30 @@
+using System;
+using System.Threading.Tasks;
+using MvvmCross.Commands;
+using Xamarin.Essentials;
+
 namespace RayHelper.ViewModels
 {
     public class RayProfilePageViewModel : MainViewModel
     {
+        public RayProfilePageViewModel()
+        {
+            OpenAboutUsLinkCommand = new MvxAsyncCommand(OpenAboutUsLinkAsync);
+        }
         
+        public IMvxAsyncCommand OpenAboutUsLinkCommand { get; set; }
+        
+        private async Task OpenAboutUsLinkAsync()
+        {
+            var webSite = "https://rayfund.ru/about/";
+            try
+            {
+                await Browser.OpenAsync(webSite);
+            }
+            catch(Exception ex)
+            {
+                // An unexpected error occured. No browser may be installed on the device.
+            }
+        }
     }
 }
