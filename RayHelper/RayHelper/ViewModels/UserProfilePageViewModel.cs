f666043e7d1bc22ba757a2a02461740e7eeea178
leorumyantsev <rumiantsev.l@gmail.com>
2021-11-18 20:55:19 +0300

    Refactor UserProfilePage.xaml

--- RayHelper/RayHelper/ViewModels/UserProfilePageViewModel.cs
+++ RayHelper/RayHelper/ViewModels/UserProfilePageViewModel.cs
@@ -8,10 +8,27 @@ namespace RayHelper.ViewModels
             get => _isUserAuthorized;
             set => SetProperty(ref _isUserAuthorized, value);
         }
+        
+        private string _userName;
+        public string UserName
+        {
+            get => _userName;
+            set => SetProperty(ref _userName, value);
+        }
+        
+        private string _login;
+        public string Login
+        {
+            get => _login;
+            set => SetProperty(ref _login, value);
+        }
 
         public UserProfilePageViewModel()
         {
             IsUserAuthorized = false;
+
+            UserName = "Петр Францевич";
+            Login = "petr_madi@rambler.ru";
         }
     }
 }
