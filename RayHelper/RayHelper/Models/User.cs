b15e566ade4800adca87bd8b412131d5375ed1cc
Leo <rumiantsev.l@gmail.com>
2021-11-23 00:06:17 +0300

    Refactor UsersRankPage and view model and add User.cs

---
+++ RayHelper/RayHelper/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RayHelper.Models
+{
+    public class User : IComparable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Rank { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+        public string RankText => $"{Rank} выполненых задач";
+        
+        public int CompareTo(object obj)
+        {
+            var user = (User) obj;
+            if (this.Rank < user.Rank)
+                return 1;
+            if (this.Rank > user.Rank)
+                return -1;
+            else
+                return String.Compare(this.LastName,user.LastName);;
+        }
+    }
+}
--- RayHelper/RayHelper/ViewModels/UsersRankPageViewModel.cs
+++ RayHelper/RayHelper/ViewModels/UsersRankPageViewModel.cs
@@ -1,7 +1,57 @@
-﻿namespace RayHelper.ViewModels
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using RayHelper.Models;
+
+namespace RayHelper.ViewModels
 {
     public class UsersRankPageViewModel : MainViewModel
     {
-        
+        private ObservableCollection<User> _users;
+        public ObservableCollection<User> Users
+        {
+            get => _users;
+            set => SetProperty(ref _users, value);
+        }
+
+        public UsersRankPageViewModel()
+        {
+            var users = new List<User>()
+                        {
+                            new User()
+                            {
+                                FirstName = "Иван",
+                                LastName = "Бругой",
+                                Rank = 5
+                            },
+                            new User()
+                            {
+                                FirstName = "Аван",
+                                LastName = "Другой",
+                                Rank = 5
+                            },
+                            new User()
+                            {
+                                FirstName = "Иван",
+                                LastName = "Другой",
+                                Rank = 3
+                            },
+                            new User()
+                            {
+                                FirstName = "Иван",
+                                LastName = "Иванов",
+                                Rank = 10
+                            }
+                        };
+            users.Sort();
+            Users = new ObservableCollection<User>(users);
+
+            //LoadData();
+        }
+
+        private void LoadData()
+        {
+            //TODO
+        }
     }
 }
